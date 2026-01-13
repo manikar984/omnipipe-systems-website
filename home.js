a951cb8c56efdad222fc1fe2e09149ed578ab432
Manikar Reddy Kothakapu <135298596+manikar984@users.noreply.github.com>
2026-01-13 22:58:41 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const section = document.querySelector(this.getAttribute('href'));
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
